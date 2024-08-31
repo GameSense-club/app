560e3e56d8b5c01b68491adbccacfdc69f63d008
Falbue <cyansair05@gmail.com>
2024-08-31 20:14:17 +0500

    Прошлый комит

--- app.py
+++ app.py
@@ -6,29 +6,63 @@ import block_key
 from taskbar import taskbar
 import number_pc
 import notification
-
+import time
 
 sio = socketio.Client()  # Инициализация клиента SocketIO
 computer_number = number_pc.get_computer_number()
 
 @sio.event
+def connect():
+    print("Успешно подключено к серверу")
+
+@sio.event
+def disconnect():
+    print("Соединение с сервером потеряно")
+
+@sio.event
+def connect_error(data):
+    print(f"Ошибка подключения: {data}")
+
+@sio.event
+def reconnect():
+    print("Повторное подключение к серверу")
+
+@sio.event
 def connect_client(data):  # Обработка события разблокировки
-    print(data)
+    print(f"Проверка: {data}")
     if data['message'] == 'unlock':
         taskbar()
-        webview.windows[0].minimize()
+        if webview.windows:
+            webview.windows[0].minimize()
         block_key.stop()
     elif data['message'] == 'block':
-        # webview.windows[0].restore()  # Развернуть окно
-        block_key.start()
-        taskbar(False)
-    
-def connect_to_server():  # Подключение к серверу
-    sio.connect(f'http://127.0.0.1:100{computer_number}')
-    print(f"Подключено к серверу 100{computer_number}")
+        if webview.windows:
+            webview.windows[0].restore()  # Развернуть окно
+        # block_key.start()
+        # taskbar(False)
+    elif data['message'] == 'notification':
+        notification.create_popup("Осталось 5 минут!")
 
-print(f"Вы ввели номер компьютера: {computer_number}")
-threading.Thread(target=connect_to_server).start()  # Фоновый поток для работы WebSocket
-client = webview.create_window('GameSense', f'http://127.0.0.1:100{computer_number}', fullscreen=True)
-connect_client({"message":"block"})
-webview.start()
+def connect_to_server():  # Подключение к серверу
+    while True:
+        if not sio.connected:
+            try:
+                sio.connect(f'http://localhost:100{computer_number}')
+                print(f"Подключено к серверу 100{computer_number}")
+                sio.wait()  # Ожидание событий
+            except Exception as e:
+                print(f"Ошибка подключения: {e}")
+                time.sleep(5)  # Задержка перед повторной попыткой подключения
+        else:
+            print("Соединение уже установлено, ожидание событий...")
+            sio.wait()
+
+def start_app():
+    print(f"Вы ввели номер компьютера: {computer_number}")
+    threading.Thread(target=connect_to_server).start()  # Фоновый поток для работы WebSocket
+    webview.create_window('GameSense', f'http://localhost:100{computer_number}', fullscreen=True)
+    webview.start()
+
+# Запуск приложения в основном потоке
+if __name__ == '__main__':
+    start_app()
--- notification.py
+++ notification.py
@@ -16,7 +16,7 @@ def create_popup(text):
     popup.geometry("260x150+{}+{}".format(screen_width - 260, 0))
 
     # Добавляем текст в окно
-    label = tk.Label(popup, text="У вас осталось 5 минут!", font=("Poppins", 16), fg="white", bg="#2a1557")
+    label = tk.Label(popup, text=text, font=("Poppins", 16), fg="white", bg="#2a1557")
     label.pack(expand=True, fill=tk.BOTH)
 
     # Скрываем окно через 5 секунд
