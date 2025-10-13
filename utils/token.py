d227aeaadf8a6742dcb852fc12f94b146a9a5c08
falbue <cyansair05@gmail.com>
2025-10-13 12:57:39 +0500

    Перенос на новое api

--- utils/token.py
+++ utils/token.py
@@ -13,19 +13,19 @@ except:
 def create_token(dir):
     file_path = os.path.join(dir, "token.txt")
     if not os.path.exists(file_path):
-        url = "https://api.game-sense.ru/pc/register"
+        url = "https://api.game-sense.ru/pc/registration"
         headers = {"Content-Type": "application/json"}
         response = requests.get(url, headers=headers)
         response_data = response.json()
-        token = response_data["token"]
+        key = response_data["key"]
         os.makedirs(dir, exist_ok=True)
         with open(file_path, 'w') as f:
-            f.write(token)
-        logger.info(f"Создан новый токен: {token}")
+            f.write(key)
+        logger.info(f"Создан новый ключ доступа: {key}")
 
     else:
         with open(file_path, 'r') as f:
-            token = f.read().strip()
-        logger.info("Используется существующий токен.")
+            key = f.read().strip()
+        logger.debug("Используется существующий ключ доступа")
 
-    return token
+    return key
